73cb08199bf09030a9b7fe371fa22405a9f11af5
FMohaddes <36656187+FMohaddes@users.noreply.github.com>
2022-04-27 02:09:55 +0430

    style home page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 50,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+let swiper2 = new Swiper(".mySwiper2", {
+  direction: "vertical",
+  spaceBetween: 50,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
