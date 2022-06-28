f34a2af66fd88b80f34b459215d88d0209481afc
FMohaddes <36656187+FMohaddes@users.noreply.github.com>
2022-06-28 17:15:48 +0430

    show latest

--- js/main.js
+++ js/main.js
@@ -41,6 +41,122 @@ $(document).ready(function(){
 });
 
 
+
+
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
+
+let swiperB = new Swiper(".mySwiperBullet", {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
+
+let swiperN = new Swiper(".mySwiperNashr", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
+
+var swiperR = new Swiper('.ravankavi-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  // autoHeight: true,
+  pagination: {
+    el: '.ravankavi-slider__pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  }
+});
+
+
 //pagination
 
 // HINT : the groups of buttons to select as visible in the pagination pannel
@@ -173,116 +289,3 @@ function range(start = 0, stop, step = 1, func = { onPush: (index) => index }) {
 
 
 
-
-let swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: 10,
-  autoplay: {
-    delay: 5500,
-    disableOnInteraction: false,
-  },
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    "@0.00": {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    "@0.75": {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    "@1.00": {
-      slidesPerView: 1,
-      spaceBetween: 40,
-    },
-    "@1.50": {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-  },
-});
-
-let swiperB = new Swiper(".mySwiperBullet", {
-  slidesPerView: 3,
-  slidesPerGroup: 3,
-  spaceBetween: 10,
-  autoplay: {
-    delay: 5500,
-    disableOnInteraction: false,
-  },
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    "@0.00": {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-
-    "@0.75": {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    "@1.00": {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    "@1.50": {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-  },
-});
-
-let swiperN = new Swiper(".mySwiperNashr", {
-  slidesPerView: 2,
-  spaceBetween: 10,
-  autoplay: {
-    delay: 5500,
-    disableOnInteraction: false,
-  },
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    "@0.00": {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    "@0.75": {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    600: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-    },
-
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-  },
-});
-
-var swiperR = new Swiper('.ravankavi-slider', {
-  spaceBetween: 30,
-  effect: 'fade',
-  loop: true,
-  // autoHeight: true,
-  pagination: {
-    el: '.ravankavi-slider__pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    },
-  }
-});
